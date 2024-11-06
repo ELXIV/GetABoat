d8bf0f820ff5bc89c1d7ecfb0fcb6b3d411f41a1
Elyza Gaile <elyzagailefabm@gmail.com>
2024-11-06 21:35:44 +0800

    wad

--- App.js
+++ App.js
@@ -12,32 +12,39 @@ export default function App() {
             <Boat name="Sea Ray 500 Sundancer"
                    description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
                    icon_name="sailboat"
-                   poster={require("./img/sea_ray.jpg")}/>
+                   poster={require("./img/sea_ray.jpg")}
+                   price="1,399"/>
+
 
             <Boat name="Four Winns Horizon 180"
                    description="A sporty look and refined details truly set the Horizon 180 above all others."
                    icon_name="sailboat"
-                   poster={require("./img/four_winns.jpg")}/>
+                   poster={require("./img/four_winns.jpg")}
+                   price="1,700"/>
 
             <Boat name="Flipper 640 ST"
                    description="A modern take on the classic, traditional hardtop and perfect for a family picnic."
                    icon_name="sailboat"
-                   poster={require("./img/flipper.jpg")}/>
+                   poster={require("./img/flipper.jpg")}
+                   price="1,900"/>
 
             <Boat name="Princess V48"
                    description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior."
                    icon_name="sailboat"
-                   poster={require("./img/princess.jpg")}/>
+                   poster={require("./img/princess.jpg")}
+                   price="2,000"/>
 
             <Boat name="Bayliner 175 Bowrider"
                   description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
                   icon_name="sailboat"
-                  poster={require("./img/bayliner.jpg")}/>
+                  poster={require("./img/bayliner.jpg")}
+                  price="1,600"/>
 
             <Boat name="Fairline Targa 47"
                   description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
                   icon_name="sailboat"
-                  poster={require("./img/fairline.jpg")}/>
+                  poster={require("./img/fairline.jpg")}
+                  price="1,500"/>
         </ScrollView>
       </View>
   );
--- components/Boat.js
+++ components/Boat.js
@@ -2,13 +2,14 @@ import {Text, View, Image, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import React from "react";
 
-const Boat = ({name, description, icon_name, poster}) => {
+const Boat = ({name, description, icon_name, poster, price}) => {
     return(
         <View style={styles.boat} >
             <Text style={styles.heading}><Icon name={icon_name} size={30}/> {name}</Text>
             <Image style={styles.image} source={poster}/>
-            <Text style={styles.text}>{description}</Text>
-            <Text>{"\n"}</Text>
+            <Text style={styles.description}>{description}</Text>
+            <Text style={styles.price}>{price}</Text>
+
         </View>
     );
 };
@@ -21,7 +22,6 @@ const styles = StyleSheet.create({
         backgroundColor: "#0a4f83",
         alignSelf: "center",
         marginVertical: 10,
-
     },
 
     heading: {
@@ -31,17 +31,26 @@ const styles = StyleSheet.create({
         color: "white",
     },
 
-    text: {
-        color: "white",
-    },
-
     image: {
         height: 300,
         width: 350,
         borderColor: "#0e73bf",
         borderWidth: 2,
         alignSelf: 'center',
+    },
+
+    description: {
+        color: "white",
+    },
+
+    price: {
+        color: "white",
+        fontSize: 30,
+        fontWeight: "bold",
+        alignSelf: "center",
     }
+
+
 });
 
 export default Boat;
